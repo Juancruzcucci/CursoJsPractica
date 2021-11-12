3887ba555e050dba91e05a4c53e4627a5e9cf192
JCGucci <juancruzcucci@gmail.com>
2021-11-12 09:18:22 -0300

    Version con cupones

--- Precios/precios.js
+++ Precios/precios.js
@@ -1,10 +1,37 @@
 
+// function calcularPrecioConDescuento(){
+//     const precioProducto = document.getElementById("inputprecio");
+//     const valorProducto = precioProducto.value;
+//     const descuento = document.getElementById("inputdescuento");
+//     const valorDescuento = descuento.value;
+//     const precioFinal = ((100 - valorDescuento) * valorProducto / 100 );
+//     const resultado = document.getElementById("resultado");
+//     resultado.innerText = "El precio final del producto es " + precioFinal;
+// };
+
+
+
 function calcularPrecioConDescuento(){
     const precioProducto = document.getElementById("inputprecio");
     const valorProducto = precioProducto.value;
-    const descuento = document.getElementById("inputdescuento");
-    const valorDescuento = descuento.value;
-    const precioFinal = ((100 - valorDescuento) * valorProducto / 100 );
-    const resultado = document.getElementById("resultado");
-    resultado.innerText = "El precio final del producto es " + precioFinal;
+    let descuentoElegido = document.getElementById("descuentoElegido");
+    // const resultado = document.getElementById("resultado");
+    
+
+    // let desc10 = document.getElementById("desc10");
+    // let desc25 = document.getElementById("desc25");
+    // let desc40 = document.getElementById("desc40");
+   
+    if(descuentoElegido.value === 'Cupon 10% descuento'){
+        const precioFinal = valorProducto * 90 /100;
+        resultado.innerText = "El precio final del producto es " + precioFinal;
+    } else if(descuentoElegido.value === 'Cupon 25% descuento'){
+        const precioFinal = valorProducto * 75 /100;
+        resultado.innerText = "El precio final del producto es " + precioFinal;
+    } else {
+        const precioFinal = valorProducto * 60 /100;
+        resultado.innerText = "El precio final del producto es " + precioFinal;
+    }
 };
+
+
